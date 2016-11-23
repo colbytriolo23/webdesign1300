e0c207891131f787bdcbced47610d4649d89a8a2
colbytriolo23 <cmt94@cornell.edu>
2016-11-22 20:33:04 -0500

    header and footer and style - 1
    here are all of the files I used in the header and footer design, you will have to put the images into a folder called images and the scripts into one called scripts, and the styling into one called styling for them to run on your computer! lmk what you think

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<<<<<<< HEAD
+<html lang="en">
+=======
+<html lang='en'>
+>>>>>>> d35dddb645bf4b77709412e4f117bd38a72bca00
+    <head>
+         <meta charset="utf-8">
+         <title> Ithaca Apple Harvest Festival </title>
+         <link rel="stylesheet" type="text/css" href="styles/main.css">
+         <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
+         <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic|Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+            <?php
+            include "scripts/header.php";
+            ?>
+            <p>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br> Mock content
+             </br>
+            </br>
+            </br> Mock content
+             </br>
+            </br>
+            </br> Mock content
+            </p>
+
+
+            
+
+            <?php 
+            include "scripts/footer.php"; 
+            ?>
+    </body>
+</html>
