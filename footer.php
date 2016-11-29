1fc579b46ef11e62ec663a9bae00d74327901ba1
colbytriolo23 <cmt94@cornell.edu>
2016-11-28 23:29:56 -0500

    NEW

--- footer.php
+++ footer.php
@@ -1,19 +1,31 @@
-<?php
-    echo '
+  <footer>
     <div id="footer_container">
       <div id="social">
-        <p> I will insert later </p>
+        <!-- Image from: https://www.iconfinder.com/icons/734386/facebook_media_online_social_icon#size=48 -->
+        <a href="http://www.facebook.com/downtownithaca">
+        <img src="images/fb.png" alt="facebook.com/downtownithaca">
+        <!-- Image from: https://www.iconfinder.com/icons/734395/instagram_media_online_photo_social_icon#size=48 -->
+        </a>
+        <a href="http://www.instagram.com/downtownithaca">
+        <img src="images/ig.png" alt="instagram.com/downtownithaca">
+        </a>  
+        <!-- Image from: https://www.iconfinder.com/icons/734367/media_online_social_twitter_icon#size=48 -->
+        <a href="http://www.twitter.com/downtownithaca">
+        <img src="images/twitter.png" alt="twitter.com/downtownithaca">
+        </a> 
+        <!-- https://www.iconfinder.com/icons/734362/media_online_social_tube_youtube_icon#size=48 -->
+        <a href="http://www.youtube.com/downtownithaca">
+        <img src="images/youtube.png" alt="youtube.com/downtownithaca">
+        </a>      
       </div>
       <div id="copyright">
         <p> Website by Jennifer Kaufman, Keyi Ruan, Colby Triolo </p>
         <p> Copyright 2016 </p>
       </div>
       <div id="contact">  
-        <p> 171 E. State Street </ br> Center Ithaca </ br> Ithaca, NY 14850 </p>
+        <p> 171 E. State Street <br/> Center Ithaca <br/> Ithaca, NY 14850 </p>
         <p> Phone: 607-277-8679 </p>
-        <p> Email: <a href="mailto: info@downtownithaca.com"> info@downtownithaca.com </a></p>
+        <p> Email: <a href="mailto:info@downtownithaca.com"> info@downtownithaca.com </a></p>
       </div>
     </div>   
-        
-    ';
-?>
+  </footer>      
--- header.php
+++ header.php
@@ -1,39 +1,44 @@
-<?php
-    echo '
-    <div id="title">
-        <h1> Ithaca Apple Harvest Festival </h1>
-    </div>
+ <head>
+         <meta charset="utf-8">
+         <title> Ithaca Apple Harvest Festival </title>
+         <link rel="stylesheet" type="text/css" href="styles/main.css">
+         <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
+         <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic|Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 
-    <div id="container">
-        <div id="nav">
-            <ul>
-                <li><a href="index.php"><strong>Home</strong></a></li>
-                <li><a href="map.php"><strong>Map</strong></a></li>
-                <li><a href="gallery.php"><strong>Gallery</strong></a></li>
-                <li><a href="calendar.php"><strong>Calendar</strong></a></li>
-                <li><a href="must-see.php"><strong>Must See</strong></a></li>
-            </ul>
-        </div>
+         <div id="title">
+           <h1> Ithaca Apple Harvest Festival </h1>
+         </div>
+
+         <div id="container">
+            <div id="nav">
+              <ul>
+                 <li><a href="index.php"><strong>Home</strong></a></li>
+                 <li><a href="map.php"><strong>Map</strong></a></li>
+                 <li><a href="gallery.php"><strong>Gallery</strong></a></li>
+                 <li><a href="calendar.php"><strong>Calendar</strong></a></li>
+                 <li><a href="must-see.php"><strong>Must See</strong></a></li>
+              </ul>
+            </div>
 
         <div id="banner_photos">
+            <div id="photo_1">
             <!-- Photo from Andrew Halper, Ithaca Apple Harvest Festival 2013 link:
             https://www.flickr.com/photos/106103661@N02/10396902555/in/album-72157648008079417/ -->
-            <div id="photo_1">
                 <img src="images/banner_1.jpg" alt="Little kid picking rocks">
             </div>
-            <!-- Photo from Andrew Halper, Ithaca Apple Harvest Festival 2015 link:
-            https://www.flickr.com/photos/106103661@N02/21839489568/in/album-72157657274645113/ -->
-            <div id="photo_2">
-                <img src="images/banner_2.jpg" alt="Wine tasting">
-            </div>
-            <!-- Photo from Andrew Halper, Ithaca Apple Harvest Festival 2014 link:
-            https://www.flickr.com/photos/106103661@N02/15266538019/in/album-72157648366747526/ -->
-            <div id="photo_3">
-                <img src="images/banner_3.jpg" alt="Little kid on grandpas shoulders">
+            
+            <div id="photos_23">
+                <div id="photo_2">
+                <!-- Photo from Andrew Halper, Ithaca Apple Harvest Festival 2015 link:
+                https://www.flickr.com/photos/106103661@N02/21839489568/in/album-72157657274645113/ -->
+                    <img src="images/banner_2.jpg" alt="Wine tasting">
+                </div>
+                <!-- Photo from Andrew Halper, Ithaca Apple Harvest Festival 2014 link:
+                https://www.flickr.com/photos/106103661@N02/15266538019/in/album-72157648366747526/ -->
+                <div id="photo_3">
+                    <img src="images/banner_3.jpg" alt="Little kid on grandpas shoulders">
+                </div>
             </div>
         </div>
     </div>
-
-    '
-    ;
-?>
+ </head>
